a585a209262cae3f9111c5a34a8322ea290b3cc5
tanacchi <q111026d@mail.kyutech.jp>
2025-05-02 16:35:29 +0900

    Remove unnecessary method

--- api/src/modules/pantry/infrastructure/mapper/pantry.orm-mapper.ts
+++ api/src/modules/pantry/infrastructure/mapper/pantry.orm-mapper.ts
@@ -1,21 +1,8 @@
 import { Pantry as PrismaPantry } from '@prisma/client';
 import { Pantry } from '../../domain/entity/pantry.entity';
 
-const checkPantry = (prismaPantry: PrismaPantry): prismaPantry is Pantry => {
-  return (
-    typeof prismaPantry === 'object' &&
-    typeof prismaPantry?.id === 'string' &&
-    typeof prismaPantry.userId === 'string' &&
-    prismaPantry.createdAt instanceof Date &&
-    prismaPantry.updatedAt instanceof Date
-  );
-};
-
 export class PantryOrmMapper {
   static toDomain(prismaPantry: PrismaPantry): Pantry {
-    if (!checkPantry(prismaPantry)) {
-      throw new Error('Invalid PrismaPantry object');
-    }
     return new Pantry(
       prismaPantry.id,
       prismaPantry.userId,
