79dd07d86630f829bef8c09899339ee15044175a
tanacchi <q111026d@mail.kyutech.jp>
2025-05-02 11:58:04 +0900

    Add access log config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ItemModule } from './modules/item/item.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [ItemModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
