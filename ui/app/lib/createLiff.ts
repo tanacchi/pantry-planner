6e6bf2d3ed1db38d57f2fa6b1e7c11307d31f544
tanacchi <q111026d@mail.kyutech.jp>
2025-05-18 22:53:44 +0900

    Use userId set on env

--- ui/app/lib/createLiff.ts
+++ ui/app/lib/createLiff.ts
@@ -1,19 +1,27 @@
-import { type Liff } from '@line/liff';
+import { type Liff } from "@line/liff";
 
-export const createLiff = async (liffId: string, useMock = false): Promise<Liff> => {
-  const liff = (await import('@line/liff')).default;
+export const createLiff = async (
+  liffId: string,
+  useMock = false
+): Promise<Liff> => {
+  const liff = (await import("@line/liff")).default;
   if (useMock) {
-    console.log('Using mock LIFF');
-    const LiffMockPlugin = (await import('@line/liff-mock')).default;
+    console.log("Using mock LIFF");
+    const LiffMockPlugin = (await import("@line/liff-mock")).default;
     liff.use(new LiffMockPlugin());
     await liff.init({
       liffId: liffId,
-      mock: true
+      mock: true,
     });
-    console.log('LIFF initialized with mock');
+    const userId = import.meta.env.VITE_DUMMY_USER_ID;
+    liff.$mock.set((p) => ({
+      ...p,
+      getProfile: { displayName: "ダミー田中", userId: userId ?? "123456789" },
+    }));
+    console.log("LIFF initialized with mock");
     return liff;
   } else {
-    const liff = (await import('@line/liff')).default;
+    const liff = (await import("@line/liff")).default;
     await liff.init({ liffId });
     return liff;
   }
